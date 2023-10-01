09f1a8f742d6603cccfa814846a8c45416ca3753
GROOTROCKET123 <146625562+GROOTROCKET123@users.noreply.github.com>
2023-10-01 14:35:42 +0530

    Add files via upload

---
+++ employee.py
@@ -0,0 +1,51 @@
+import mysql.connector 
+
+db = mysql.connector.connect(host = 'localhost', user='root',
+                             database='rms',\
+                             passwd='1234',\
+                             auth_plugin = 'mysql_native_password')
+
+a = db.cursor()
+d,c,f,e="ameer","ceo",23000,1
+
+#To create database
+'''a.execute('create database rms')'''
+
+#To create table
+'''a.execute('create table employee(EID int(3) primary key,ENAME varchar(50),DEPARTMENT VARCHAR(15),SALARY INT(15))')
+'''
+
+class update_emp():
+    #To update Name of an Employee
+    def name(EID,NAME):
+        a.execute('update employee set ENAME = %s where EID = %s',(NAME,EID))
+        db.commit()
+    #To Change Department of an Employee
+    def department(EID,DEPT):
+        a.execute('update employee set DEPARTMENT = %s where EID = %s',(DEPT,EID))
+        db.commit()
+    #To change salary of an employee
+    def salary(EID,SALARY):
+        a.execute('update employee set salary = %s where EID = %s',(SALARY,EID))
+        db.commit()
+    #To change Employee ID of an employee
+    def EID(current_EID,new_EID):
+        a.execute('update employee set EID = %s where EID = %s',(new_EID,current_EID))
+        db.commit()
+    #To increase/decrease salary of an Employee
+    '''to decrease add minus(-) sign before value'''
+    def salaryinc(EID,sal):
+        a.execute('update employee set salary = salary+%s where EID = %s',(sal,EID))
+        db.commit()
+        
+
+def add_emp(d,c,f):
+    a.execute("insert into employee(ENAME,DEPARTMENT,salary) values(%s,%s,%s)",(d,c,f))
+    db.commit()
+
+def del_emp(EID):
+    a.execute("delete from employee where EID = (%s)",(EID,))
+    db.commit()
+
+while True:
+    print('*'*100)
---
+++ gui.py
@@ -0,0 +1,57 @@
+from tkinter import *
+from tkinter import Menu
+#import employee as e
+from PIL import ImageTk,Image
+
+'''def l():
+    n = event.width
+    k = event.height
+    home = hc.resize((n,k))
+    home_icon = ImageTk.PhotoImage(home)
+    m.configure(image = home_icon)'''
+    
+
+a = Tk()
+#code to insert title
+a.title('RESTAURANT MANAGEMENT SYSTEM')
+#code to insert icon
+a.iconbitmap('images/logo.ico')
+#code to give size of the form
+a.geometry('1000x600')
+#a.resizable(0,0)
+
+home = Image.open('images/home.png')
+home = home.resize((50,50))
+home_icon = ImageTk.PhotoImage(home)
+
+
+
+sideBarLeft = Frame(a,bg = "black")
+sideBarRight = Frame(a,bg = "red")
+upperBar = Frame(a,bg = "yellow")
+
+sideBarLeft.place(relx=0,rely=0.11,relwidth = 0.225,relheight = 1)
+upperBar.place(relx=0,rely=0,relwidth = 2,relheight = 0.11)
+sideBarRight.place(relx=0.9,rely=0.11,relwidth=1,relheight=1)
+
+home_btn = Button(a,image = home_icon)
+home_btn.pack()
+
+'''m = Menu(a)
+f = Menu(m,tearoff = 0)
+m.add_cascade(label = "home",image= home_icon,menu = f)
+f.add_command(label='Exit',command = a.destroy)'''
+
+
+
+'''m=Menubutton(a,text = "home",image = home_icon)
+m.place(relx=0.5,rely = 0.5)
+
+
+h = Menu(m)
+m.config(menu=h)
+#Menu(a).add_cascade(label = 'file',menu = f)
+#a.config(menu = m)'''
+
+
+a.mainloop()
